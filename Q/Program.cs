d980a0acd6e79dc117db8ca352008e4ec79b3e7a
Tony <Tony@TONY>
2022-10-22 00:33:36 +0800

    加入專案檔案。

---
+++ Q/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1+2+3+4+5+...+n<105
+            int value = 0;
+            int result = 0;
+            while (result < 105)
+            {
+                value++;
+                result += value;
+            }
+            
+            Console.WriteLine(value - 1);
+
+
+            //105-1-2-3...-n<0
+
+            //int value2 = 0;
+            //int result2 = 105;
+            //while (result2>0)
+            //{
+            //    value2++;
+            //    result2 -= value2;
+
+            //}
+            //Console.WriteLine(value2 - 1);
+
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用while,在畫面顯示所有可以整除300的整數
+            //例如:1,2,3,4,5,6,10,...,300
+            int num1 = 1;
+
+
+            while ( num1 <=300 )
+            {
+                if(300 % num1 == 0)
+                {
+                    Console.WriteLine(num1);
+                }
+                num1++;                
+            }
+        }
+    }
+}
